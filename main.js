54ff5483fd6f9047ae694d0afe5483b7584ae594
abestract <ibrahim.iqbal@gmail.com>
2014-12-04 14:56:44 -0800

    JS and CSS updates...

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-//Wrapper element for our loader.
+//Wrapper elements for our loader.
 var wrapper = "<div class=\"wrapper\"></div>";
-
+var loaderPx = "<span class=\"loader-px\"></span>";
 
 //Append the wrapper dynamically to the body element.
 $(document).ready(function () {
   $("body").on("click", function(){
     $(this).append(wrapper);
-      //Get the width of the wrapper element after it's been appended
-      var appendWrapperWidth = $(".wrapper").width();
+    //Get the width of the wrapper element after it's been appended
+    var appendWrapperWidth = $(".wrapper").width();
+    var applyLoader = function () {
+      //Loop thru and apply 1px to the wrapper
+      for (var i=0; i<appendWrapperWidth; i++) {
+        $(".wrapper").append(loaderPx);
+      }
+    };
+    return applyLoader();
   });
 });
 
