66c1c835abaab6490d309fb076d04a98504f9795
abestract <ibrahim.iqbal@gmail.com>
2014-12-05 09:59:57 -0800

    JS, and CSS updates...

--- main.js
+++ main.js
@@ -8,13 +8,15 @@ $(document).ready(function () {
     $(this).append(wrapper);
     //Get the width of the wrapper element after it's been appended
     var appendWrapperWidth = $(".wrapper").width();
-    var applyLoader = function () {
-      //Loop thru and apply 1px to the wrapper
-      for (var i=0; i<appendWrapperWidth; i++) {
+    //Using this method: http://scottiestech.info/2014/07/01/javascript-fun-looping-with-a-delay/
+    (function theLoop (i) {
+      setTimeout(function () {
         $(".wrapper").append(loaderPx);
-      }
-    };
-    return applyLoader();
+        if (--i) {          // If i > 0, keep going
+          theLoop(i);       // Call the loop again, and pass it the current value of i
+        }
+      }, 1);
+    })(appendWrapperWidth);
   });
 });
 
